2d2ca9089cc2bcd0d0df50cba8ab15e25552f61c
Lichun Zhan <lzhan@yahoo-inc.com>
2013-07-03 10:11:45 -0700

    add unit tests for core.js

--- tests/lib/index.js
+++ tests/lib/index.js
@@ -14,17 +14,51 @@ var libpath = require('path'),
     libfs = require('fs'),
     mockery = require('mockery'),
     expect = require('chai').expect,
-    plugin = require('../../lib/plugin.js');
+    assert = require('chai').assert,
+    core = require('../../lib/core.js'),
+    plugin = require('../../lib/plugin.js'),
+    libpromise  = require('yui/promise'),
+    liblocator = require('locator'),
+    fixturesPath = libpath.join(__dirname, '../fixtures');
 
-describe('plugin', function () {
+describe('locator-handlebars', function () {
 
-    describe('instance', function () {
+    describe('plugin', function () {
 
         // we nee to write some tests here!
         it('partialDir', function () {
             expect(plugin.describe.partialDir).to.equal('./partials');
         });
 
+    });
+
+    describe('core', function () {
+
+        it('isPartial', function () {
+            var result = core.isPartial("../../lib", "../..");
+            expect(result).to.equal(true);
+        });
+
+        it('isPartialnegative1', function () {
+            var result = core.isPartial("../../lib", "../../lib");
+            expect(result).to.equal(false);
+        });
+
+        it('isPartialnegative2', function () {
+            var result = core.isPartial("../../lib", "../../lib/core.js");
+            expect(result).to.equal(false);
+        });
+
+        it('partials', function () {
+            var result = core.partials(fixturesPath + '/testfile.handlebars');
+            expect(result[0]).to.equal('baz');
+            expect(result[1]).to.equal('abcd');
+        });
+
+        it('compile', function () {
+            var result = core.compile(fixturesPath + '/testfile.hb');
+            expect(JSON.stringify(result).substring(1, 51)).to.equal('function (Handlebars,depth0,helpers,partials,data)');
+        });
     });
 
 });
